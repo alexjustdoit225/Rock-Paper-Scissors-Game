d19c90875b6e97e4822bff79c11d5dc5dc7405ee
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-19 12:17:07 -0500

    removed for loop
    - did not need

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //console.log('Hello World')
 function computerPlay() {
     // create an array storing rock, paper, scissors
-    // create a function that loops through the array
+    const choices = ['rock', 'paper', 'scissors']; 
         // create a variable that stores a random number between 0 and 2
-        // create a variable choice, that uses the random number variable to pick rock, paper, or scissors in the array 
+        let randNum = Math.floor(Math.random()*2); 
+        // create a variable choice, that uses the random number variable to pick rock, paper, or scissors in the array
+        let choice = choices[]
+    
     return choice; //choice should return rock, paper, or scissors 
 }
