a29517fdd712832060b02047c8bbb81c26545cfb
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-19 12:36:17 -0500

    created function oneRound()
    - will play one round of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //console.log('Hello World')
 function computerPlay() {
     // create an array storing rock, paper, scissors
-    const choices = ['rock', 'paper', 'scissors']; 
+    const choices = ['Rock', 'Paper', 'Scissors']; 
     // create a variable that stores a random number between 0 and 2
     let randNum = Math.floor(Math.random()*3); 
     // create a variable choice, that uses the random number variable to pick rock, paper, or scissors in the array
@@ -11,3 +11,8 @@ function computerPlay() {
     return choice; //choice should return rock, paper, or scissors 
 }; 
 computerPlay(); 
+
+// write a function that plays one round of rock, paper, scissors
+function oneRound() {
+    
+}
