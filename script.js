907622f6132a5334e3c0cde31572f1c7ae55792f
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-19 12:31:45 -0500

    computerPlay() complete
    - a function that makes the computer's choice at random and returns rock, paper, or scissors

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 function computerPlay() {
     // create an array storing rock, paper, scissors
     const choices = ['rock', 'paper', 'scissors']; 
-        // create a variable that stores a random number between 0 and 2
-        let randNum = Math.floor(Math.random()*2); 
-        // create a variable choice, that uses the random number variable to pick rock, paper, or scissors in the array
-        let choice = choices[]
+    // create a variable that stores a random number between 0 and 2
+    let randNum = Math.floor(Math.random()*3); 
+    // create a variable choice, that uses the random number variable to pick rock, paper, or scissors in the array
+    let choice = choices[randNum]; 
     
+    console.log(choice); 
     return choice; //choice should return rock, paper, or scissors 
-}
+}; 
+computerPlay(); 
